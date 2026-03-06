e8977710d174b1467f3b34cf280db6aabc9530f9
jonathan hagendoorn <jhagendo@calpoly.edu>
2026-03-06 13:51:46 -0800

    update contract yet again, i think (hope) this is the last one

--- Assets/Scripts/PlayerInteractableGrid.cs
+++ Assets/Scripts/PlayerInteractableGrid.cs
@@ -6,5 +6,10 @@ public abstract class PlayerInteractableGrid : MonoBehaviour
     protected Vector2 position = Vector2.zero;
     protected bool isFocused = false;
 
-    public abstract void ShiftFocusPosition(Vector2 direction);
+    /// <summary>
+    /// shifts the grid's focus in a given direction
+    /// </summary>
+    /// <param name="direction">the direction to shift in -- probably unit vector?</param>
+    /// <returns>Vector2.zero if you are still within the grid, and a direction of which edge you are leaving from if you are at the edge</returns>
+    public abstract Vector2 ShiftFocusPosition(Vector2 direction);
 }
