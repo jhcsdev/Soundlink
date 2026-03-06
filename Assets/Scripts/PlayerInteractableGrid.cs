e1e665151ac109bbeece14329b4f1575bd438aa3
jonathan hagendoorn <jhagendo@calpoly.edu>
2026-03-06 13:41:54 -0800

    update contract x2, mabe this is a bad architecture decision

--- Assets/Scripts/PlayerInteractableGrid.cs
+++ Assets/Scripts/PlayerInteractableGrid.cs
@@ -6,5 +6,5 @@ public abstract class PlayerInteractableGrid : MonoBehaviour
     protected Vector2 position = Vector2.zero;
     protected bool isFocused = false;
 
-    protected abstract void ShiftFocusPosition(Vector2 direction);
+    public abstract void ShiftFocusPosition(Vector2 direction);
 }
