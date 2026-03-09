47053f53251f9d660f2d6be0e4aa006d6256cb78
James Irwin <jamesirwin0211@gmail.com>
2026-03-08 17:10:34 -0700

    remove unnecessary comment

--- Assets/Scripts/pieces/Piece.cs
+++ Assets/Scripts/pieces/Piece.cs
@@ -4,8 +4,6 @@ namespace GamePieces
 {
     public class Piece : MonoBehaviour
     {    
-        // TODO: I don't really think it makes sense for piece to have location data since a piece may
-        // occupy multiple spaces on the board
         public void InventoryMode()
         {
             
