ad45b0f56a6057130c3ad816024441f70294c04a
James Irwin <jamesirwin0211@gmail.com>
2026-04-29 15:32:37 -0700

    new beat playback button

--- Assets/Scripts/buttons/ReferenceButton.cs
+++ Assets/Scripts/buttons/ReferenceButton.cs
@@ -22,6 +22,7 @@ public class ButtonAction : MonoBehaviour
 
     void Start()
     {
+
         myButton.onClick.AddListener(LogClick);
 
         // initialize text
@@ -32,53 +33,27 @@ public class ButtonAction : MonoBehaviour
 
         // must declare Chuck events before creating listeners
         myChuck.RunCode( string.Format( @"
-            global Event beatStart;
-            global Event beatDone;
+            global Event playBeat;
+            global Event pauseBeat;
         "));
 
-        // Debug.Log($"myChuck is (RefenceButton): {myChuck}");
-        // Debug.Log($"buttonText is: {buttonText}");
-
-        // create listener for when beat starts
-        // create a ChuckEventListener, call SetButtonPlaying() during Update() after every broadcast from "beatStart"
-        ChuckEventListener beatStartListener = gameObject.AddComponent<ChuckEventListener>();
-        beatStartListener.ListenForEvent( myChuck, "beatStart", SetButtonPlaying);
-
-        // create listener for when beat ends
-        // create a ChuckEventListener, call SetButtonIdle() during Update() after every broadcast from "beatDone"
-        ChuckEventListener beatDoneListener = gameObject.AddComponent<ChuckEventListener>();
-        beatDoneListener.ListenForEvent( myChuck, "beatDone", SetButtonIdle);
+        sound?.PlaySound();
     }
 
     void LogClick()
     {
-        sound?.PlaySound();
-        ToggleIcon();
-    }
-
-    void ToggleIcon()
-    {
         isPlaying = !isPlaying;
+
+        // toggle sprite
         buttonImage.sprite = isPlaying ? pauseSprite : playSprite;
-    }
 
-    // disable link sound playback, change button visuals
-    void SetButtonPlaying()
-    {
-        linkManager.DisableSoundPlayback();
-
-        ColorBlock colors = myButton.colors;
-        colors.disabledColor = playingColor;
-        myButton.colors = colors;
-    }
-
-    // enable link sound playback, change button visuals
-    void SetButtonIdle()
-    {
-        linkManager.EnableSoundPlayback();
-
-        ColorBlock colors = myButton.colors;
-        colors.normalColor = idleColor;
-        myButton.colors = colors;
+        if (isPlaying)
+        {
+            myChuck.BroadcastEvent("playBeat");
+        } 
+        else
+        {
+            myChuck.BroadcastEvent("pauseBeat");
+        }
     }
 }
