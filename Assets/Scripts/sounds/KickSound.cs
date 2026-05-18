e72e0c556a8d2c87cb8087ba667ae804b08635e3
irwinj22 <jamesirwin0211@gmail.com>
2026-05-18 12:11:46 -0700

    metronome appears to be workign

--- Assets/Scripts/sounds/KickSound.cs
+++ Assets/Scripts/sounds/KickSound.cs
@@ -17,7 +17,6 @@ namespace TrackSounds
 
             myChuck.RunCode( string.Format( @"
             global float BPM;
-            global Event playMetronomeSingleSound;
 
             SinOsc kick => ADSR envKick => Gain kickGain => dac;
 
@@ -29,7 +28,6 @@ namespace TrackSounds
             (60.0 / BPM)::second => dur beat_dur;
 
             fun void playKick(float beat_note) {{
-                playMetronomeSingleSound.broadcast();
                 // turn kick on
                 KICK_GAIN => kick.gain;
                 
