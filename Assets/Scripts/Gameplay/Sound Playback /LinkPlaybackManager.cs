f114da949b9117a163ddf2ef86289d9b13f11c74
jonathan hagendoorn <jhagendo@calpoly.edu>
2026-06-01 11:31:57 -0700

    prepare for webgl build

--- Assets/Scripts/Gameplay/Sound Playback /LinkPlaybackManager.cs
+++ Assets/Scripts/Gameplay/Sound Playback /LinkPlaybackManager.cs
@@ -7,8 +7,6 @@ using UnityEngine;
 using TrackSounds;
 using ChuckChuckChuck;
 using SceneTransition;
-using UnityEditor.XR;
-using Unity.VisualScripting;
 
 namespace GridLinks
 {
@@ -134,12 +132,13 @@ namespace GridLinks
                 global Event pauseReference;
             "));
 
-            // TODO: get the level data dynamically
-            // TODO: this should actually level data (levelloader.getreferencebeat)
-            TrackSound referenceSound = LevelLoader.instance.GetReferencePlayback();
+            if (LevelLoader.instance.GetGridData() != null) {
+                TrackSound referenceSound = LevelLoader.instance.GetReferencePlayback();
 
-            // intialize those events 
-            referenceSound?.PlaySound();
+                // intialize those events 
+                referenceSound?.PlaySound();
+            }
+
 
             // if playback is already enabled in inspector, sync to metronome on start
             if (soundPlaybackEnabled)
